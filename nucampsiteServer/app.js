dd036601803b33f31ee4e3de21a9c13064c85dc6
Bobby Keel <bobbykeel76@gmail.com>
2021-09-07 17:54:00 -0500

    filesstore, express session

--- nucampsiteServer/app.js
+++ nucampsiteServer/app.js
@@ -4,6 +4,8 @@ var express = require('express');
 var path = require('path'); //core module built into nodejs
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session');
+const FileStore = require('session-file-store')(session);
 
 //importing local files
 var indexRouter = require('./routes/index');
@@ -54,10 +56,20 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('12345-67890-09876-54321'));
+// app.use(cookieParser('12345-67890-09876-54321'));
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
+
 // user authentication
 function auth(req, res, next) {
-  if(!req.signedCookies.user) {
+  console.log(req.session);
+  if(!req.session.user) {
     const authHeader = req.headers.authorization;
     if(!authHeader){
       const err = new Error('You are not authenticated!');
@@ -70,7 +82,7 @@ function auth(req, res, next) {
     const user = auth[0];
     const pass = auth[1];
     if (user === 'admin' && pass ==='password') {
-      res.cookie('user', 'admin', {signed:true});
+      req.session.user = 'admin';
       return next(); // authorized
     } else {
       const err = new Error('You are not authorized!');
@@ -79,7 +91,7 @@ function auth(req, res, next) {
       return next(err);
     }
   } else {
-    if(req.signedCookies.user === 'admin') {
+    if(req.session.user === 'admin') {
       return next();
     } else {
       const err = new Error('You are not authenticated!')
