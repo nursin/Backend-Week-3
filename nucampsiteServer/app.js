3ab44b19bf116a9b5118346940096f789e89e80f
Bobby Keel <bobbykeel76@gmail.com>
2021-09-07 13:15:43 -0500

    basic authorization

--- nucampsiteServer/app.js
+++ nucampsiteServer/app.js
@@ -55,6 +55,32 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+// user authentication
+function auth(req, res, next) {
+  console.log(req.headers);
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+    const err = new Error('You are not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+  }
+
+  const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  const user = auth[0];
+  const pass = auth[1];
+  if (user === 'admin' && pass ==='password') {
+    return next(); // authorized
+  } else {
+    const err = new Error('You are not authorized!');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+  }
+}
+
+app.use(auth);
+
 // to let express know where they are located
 // the diretory that holds static content
 // this is where usually store images, videos
